8f5fdf08029b4c1c62b073990b2287e1b992e82d
byungsang <jo_bs@promartians.com>
2023-01-07 17:45:31 +0900

    Initial commit

---
+++ src/main/java/study/searchChattingData/SearchChattingDataApplication.java
@@ -0,0 +1,13 @@
+package study.searchChattingData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchChattingDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchChattingDataApplication.class, args);
+	}
+
+}
